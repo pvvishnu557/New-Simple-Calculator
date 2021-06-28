a7a6744c5210d01fb9d79e813363f0f90553f277
pvvishnu557 <71384832+pvvishnu557@users.noreply.github.com>
2021-06-28 15:11:55 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,30 @@
+def print_menu():
+    print("1.addition")
+    print("2.subtration")
+    print("3.multiplication")
+    print("4.divition")
+
+choice=True
+while(choice):
+    num=float(input("enter the first number:"))
+    num2=float(input("enter the second number:"))
+    print_menu()
+    z=int(input("enter your choice:"))
+    if z==1:
+        print("the answer is",num+num2)
+    elif z==2:
+        print("the answer is",num-num2)
+    elif z==3:
+        print("the answer is",num*num2)
+    elif z==4:
+        try:
+            print("the answer is",num/num2)
+        except:
+            print("not divisible by zero")
+    else:
+        print("enter a valid input")
+        continue
+    again=input("want to do again:\n"
+            +"yes or no:")
+    if again=="n"or "N":
+        choice=False
